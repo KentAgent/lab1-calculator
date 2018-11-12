d66e4ad869d6b4f437ff489990b4b1717c431216
KentAgent <georgios.apostolidis92@gmail.com>
2018-11-12 21:46:41 +0100

    Initial commit

---
+++ Calculator/CalculatorObject.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class CalculatorObject
+    {
+
+        public int firstNumber{ get; set; }
+        public int secondNumber{ get; set; }
+        float numberInMemory;
+
+        public CalculatorObject()
+        {
+            clean();
+        }
+
+        private void clean()
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            numberInMemory = 0;
+        }
+
+        internal int sum()
+        {
+            return firstNumber + secondNumber;
+        }
+
+        internal float multiply()
+        {
+            return firstNumber * secondNumber;
+        }
+
+        internal float sumInBetweenNumbers()
+        {
+            int n1 = firstNumber;
+            int n2 = secondNumber;
+
+            if (firstNumber > secondNumber) 
+            {
+                n1 = secondNumber;
+                n2 = firstNumber;
+            }
+
+            int total = 0;
+
+            for (int i = n1; i < n2 + 1; i++)
+            {
+                total += i;
+            }
+
+            return total;
+        }
+
+        internal void storeNumber(int number)
+        {
+            numberInMemory = number;
+        }
+
+        internal void removeFromMemory()
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            numberInMemory = 0;
+        }
+
+        internal string printToString()
+        {
+            string message = @"
+                First number: {0}
+                Second number: {1}
+                Number in memory: {2}";
+            return string.Format(message, firstNumber, secondNumber, numberInMemory);
+        }
+
+        internal void setNumbers(int n1, int n2)
+        {
+            firstNumber = n1;
+            secondNumber = n2;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Calculator
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            CalculatorObject calculator = new CalculatorObject();
+            Console.WriteLine("Welcome to your brand new calculator");
+
+            while(true) 
+            {
+                int option = startMenu();
+                options(calculator, option);
+            }
+        }
+
+        private static void options(CalculatorObject calculator, int option)
+        {
+            switch (option)
+            {
+                case 1:
+                    inputTwoNumbers(calculator);
+                    Console.WriteLine("The sum is: " + calculator.sum());
+                    break;
+                case 2:
+                    inputTwoNumbers(calculator);
+                    Console.WriteLine("The product is: " + calculator.multiply());
+                    break;
+                case 3:
+                    inputTwoNumbers(calculator);
+                    Console.WriteLine("The sum is : " + calculator.sumInBetweenNumbers());
+                    break;
+                case 4:
+                    string s = Console.ReadLine();
+                    calculator.storeNumber(Convert.ToInt32(s));
+                    Console.WriteLine("Successfully added number to memory");
+                    break;
+                case 5:
+                    calculator.removeFromMemory();
+                    Console.WriteLine("Successfully removed number from memory");
+                    break;
+                case 6:
+                    Console.WriteLine(calculator.printToString());
+                    break;
+                case 7:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+        }
+
+        private static void inputTwoNumbers(CalculatorObject calculator)
+        {
+            string firstInput = null;
+            string secondInput = null;
+
+            do
+            {
+                Console.WriteLine("Give me two numbers");
+
+                firstInput = Console.ReadLine();
+                secondInput = Console.ReadLine();
+            } while (firstInput == "" || secondInput == "");
+
+            calculator.setNumbers(Convert.ToInt32(firstInput), Convert.ToInt32(secondInput));
+        }
+
+        private static int startMenu() 
+        {
+            string menuOptions = @"
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                1. Sum two numbers together
+                2. Multiply two numbers together
+                3. Sum the numbers between two numbers
+                4. Save number to calculator memory
+                5. Clear calculator memory
+                6. Print calculator content
+                7. Exit
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                ";
+            Console.WriteLine(menuOptions);
+            var userInput = Convert.ToInt32(Console.ReadLine());
+            return userInput;
+        }
+    }
+}
